b96bd3c9336dcd8fa50a133bae3449ac0a802558
UtpalMaiti <Utpalmaiti12@gmail.com>
2023-07-14 12:02:21 +0530

    TT

---
+++ Microsoft Business Intelligence – MSBI/MSBI_Project-Tabular-SSIS-MultidimensionalProject-PowerPivot/run_ssis_package_Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.IntegrationServices;
+
+namespace run_ssis_package_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string targetServerName = "localhost";
+            string folderName = "Project1Folder";
+            string projectName = "Integration Services Project1";
+            string packageName = "Package.dtsx";
+
+            // Create a connection to the server
+            string sqlConnectionString = "Data Source=" + targetServerName +
+                ";Initial Catalog=master;Integrated Security=SSPI;";
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionString);
+
+            // Create the Integration Services object
+            IntegrationServices integrationServices = new IntegrationServices();
+
+            // Get the Integration Services catalog
+            Catalog catalog = integrationServices.Catalogs["SSISDB"];
+
+            // Get the folder
+            CatalogFolder folder = catalog.Folders[folderName];
+
+            // Get the project
+            ProjectInfo project = folder.Projects[projectName];
+
+            // Get the package
+            PackageInfo package = project.Packages[packageName];
+
+            // Run the package
+            package.Execute(false, null);
+
+        }
+    }
+}
